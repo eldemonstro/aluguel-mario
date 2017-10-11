bfb32732d8b8fea47382aad0eab2321c543c3354
Nicolas e Luis <ncs.cr4sh@gmail.com>
2017-10-11 19:20:07 -0300

    install gem annotate

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,2 +1,22 @@
+# == Schema Information
+#
+# Table name: properties
+#
+#  id                :integer          not null, primary key
+#  location          :string
+#  area              :string
+#  title             :string
+#  description       :text
+#  daily_rate        :decimal(, )
+#  rooms             :integer
+#  minimum_rent_days :integer
+#  maximum_rent_days :integer
+#  photo             :string
+#  maximum_occupancy :integer
+#  usage_rules       :text
+#  created_at        :datetime         not null
+#  updated_at        :datetime         not null
+#
+
 class Property < ApplicationRecord
 end
