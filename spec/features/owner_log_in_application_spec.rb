282b60c3fdd3483a41781ae015e95988f75d6fd1
Virginia e Henrique <riquemorato@gmail.com>
2017-10-26 21:49:13 -0200

    Implemented tests for owner log in

---
+++ spec/features/owner_log_in_application_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+feature 'owner log in application' do
+  scenario 'successfully' do
+    user = create(:user, email: 'teste@teste.com', password: '12345')
+
+    visit root_path
+    click_on 'Entrar'
+
+    fill_in 'Email' with: user.email
+    fill_in 'Senha' with: '12345'
+
+    expect(page).to have_content("Bem vindo, #{user.name}")
+    expect(page).not_to have_content("Entrar")
+    expect(page).to have_content("Sair")
+  end
+end
---
+++ spec/mailers/previews/proposal_mailer_preview.rb
@@ -0,0 +1,19 @@
+class ProposalMailerPreview < ActionMailer::Preview
+  def accept_proposal
+    property_type = PropertyType.create(name: 'Apartamento na praia')
+
+    property = Property.create(title: 'Sitio legal', area: '300m2',
+                    location: 'Juquitiba', description: 'Bem grande',
+                    daily_rate: 800.55, rooms: 12, minimum_rent_days: 3,
+                    maximum_rent_days: 14, photo: 'photo_boa2.jpg',
+                    maximum_occupancy: 15, usage_rules: 'Não pode criança',
+                    property_type: property_type)
+
+    proposal = Proposal.create(user_name: 'Antonio', email: 'a@a.com',
+                                 start_date: '01/01/2017', end_date: '07/01/2017',
+                                 total_guests: 5, purpose: 'ferias da familia',
+                                 property: property, accept_usage_rules: true)
+
+    ProposalMailer.accept_proposal(proposal)
+  end
+end
