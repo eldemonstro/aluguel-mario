c243a449d75bdb618f20d7c3730f7984cdd4f9d7
Virginia e Henrique <riquemorato@gmail.com>
2017-10-26 20:54:06 -0200

    Implemented mail body

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171020000111) do
+ActiveRecord::Schema.define(version: 20171026221811) do
 
   create_table "properties", force: :cascade do |t|
     t.string "location"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20171020000111) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "property_type_id"
+    t.text "address"
     t.index ["property_type_id"], name: "index_properties_on_property_type_id"
   end
 
--- spec/mailers/proposal_mailer_spec.rb
+++ spec/mailers/proposal_mailer_spec.rb
@@ -1,4 +1,5 @@
 require 'rails_helper'
+include ActionView::Helpers::NumberHelper
 
 describe ProposalMailer, type: :mailer do
   describe "accept_proposal" do
@@ -21,10 +22,30 @@ describe ProposalMailer, type: :mailer do
       property = create(:property, property_type: property_type)
 
       proposal = create(:proposal, property: property)
+      proposal.total_amount_calculator
+
+      name = 'João da Silva'
 
       mail = ProposalMailer.accept_proposal(proposal)
-      expect(mail.body).to match /Pode programar sua viagem./
 
+      p_1 = %'Eu, João da Silva, portador(a) do CPF nº 111.222.333-44, declaro para os devidos
+fins que alugo o imóvel sito à #{proposal.property.address}, para Maria da Silva,
+portador(a) do CPF nº 555.666.777-88, entre o período de
+#{I18n.localize proposal.start_date} a #{I18n.localize  proposal.end_date}, pelo valor total de #{number_to_currency proposal.total_amount}.'
+
+      p_2 = %'Para este período devem ser respeitados as seguintes regras de uso:
+O imóvel não deve ser hospedado por mais que #{proposal.property.maximum_occupancy} pessoas;
+O prazo de locação deve ser respeitado e casos de renovações devem ser
+tratados diretamente com o proprietário;
+Além destas, devem ser respeitadas também as seguintes regras:
+#{proposal.property.usage_rules}'
+
+      p_3 = "#{I18n.localize Date.today, format: :long}"
+
+      expect(mail.body).to match("Comprovante de Locação")
+      expect(mail.body).to include p_1
+      expect(mail.body).to include p_2
+      expect(mail.body).to include p_3
     end
   end
 end
